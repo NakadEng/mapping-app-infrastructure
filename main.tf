a5cf8e73dd47981a3d7a49f6cb6e60fcd74f7682
NakadEng <umuneko@gmail.com>
2019-04-05 15:12:16 +0900

    fix add security group

--- main.tf
+++ main.tf
@@ -68,13 +68,44 @@ resource "aws_security_group" "admin" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "http-https-express" {
+    name = "http-https-express"
+    description = "Allow HTTP-HTTPS-Express inbound traffic"
+    vpc_id = "${aws_vpc.test-mappingApp.id}"
+    ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
  
 resource "aws_instance" "mappingApp-ba" {
     ami = "${lookup(var.images, "ap-northeast-2")}"
     instance_type = "t2.micro"
     key_name = "key_nakata_kr"
     vpc_security_group_ids = [
-      "${aws_security_group.admin.id}"
+      "${aws_security_group.admin.id}",
+      "${aws_security_group.http-https-express.id}"
     ]
     subnet_id = "${aws_subnet.public-a.id}"
     associate_public_ip_address = "true"
